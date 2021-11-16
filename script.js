239a0e6b836453ab562d0fc8a898affcceace7da
luna <lunashoval@gmail.com>
2021-11-16 04:10:28 -0500

    script.js

--- script.js
+++ script.js
@@ -7,12 +7,20 @@ function Book(name, author, pages, read) {
   this.pages = pages;
   this.read = read;
 }
+let harryPotter = new Book("harryPotter" + "jk" + 300, "nope");
+let theguy = new Book("theguy", "luna", 300, "yes");
+
 const container = document.querySelector(".container");
+
 function addBookToLibrary(book) {
     myLibrary.push(book);
 }
+addBookToLibrary(harryPotter);
+addBookToLibrary(theguy);
+
 for(i = 0; i < myLibrary.length; i++){
     let newCard = document.createElement("div");
     newCard.textContent = myLibrary[i].name + " " + myLibrary[i].author + " " +myLibrary[i].pages + " " + myLibrary[i].read
     newCard.appendChild(container);
 }
+
