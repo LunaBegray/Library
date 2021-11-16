2ddd3228f9fdc69e9deedf8a85d85506501042e2
luna <lunashoval@gmail.com>
2021-11-16 18:03:42 -0500

    script.js

--- script.js
+++ script.js
@@ -19,16 +19,36 @@ addBookToLibrary(harryPotter);
 console.log(myLibrary);
 addBookToLibrary(theguy);
 console.log(myLibrary);
-/*
+
 let addBtn = document.querySelector(".addBtn");
 addBtn.addEventListener('click', function(){
     let form = document.createElement("div");
     form.classList.add("form");
-
-    
+    let inputName = document.createElement("input")
+    let inputAuthor = document.createElement("input")
+    let inputPages = document.createElement("input")
+    let inputRead = document.createElement("input")
+    form.appendChild(inputName);
+    form.appendChild(inputAuthor);
+    form.appendChild(inputPages);
+    form.appendChild(inputRead);
+    let continueBtn = document.createElement("button");
+    continueBtn.addEventListener('click', function(){
+        let nameFNB = inputName.value;
+        let authorFNB  = inputAuthor.value;
+        let pagesFNB = inputPages.value;
+        let readFNB = input.value;
+        let newBook = new Book(nameFNB, authorFNB, pagesFNB, readFNB);
+        addBookToLibrary(newBook);
+        console.log(newBook);
+        console.log(myLibrary);
+        form.remove();
+    });
+    form.appendChild(continueBtn);
+    container.appendChild(form);
 }
-) 
-*/
+); 
+
 for(i = 0; i < myLibrary.length; i++){
     let newCard = document.createElement("div");
     newCard.classList.add("card");
